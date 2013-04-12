0c359e2cfae55113416027edd954381b5e111848
Rodrigo Dumont <rlsdumont@gmail.com>
2013-04-11 23:49:58 -0300

    Adds assembly info

---
+++ src/CommandLine/Properties/SharedAssemblyVersion.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyDescription("A static site generator, best suited for blogs")]
+[assembly: AssemblyCompany("Rodrigo Dumont")]
+[assembly: AssemblyProduct("Frankie")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
