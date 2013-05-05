82ddbe3184c2690280ae6120f5e0f911598a568c
Rodrigo Dumont <rlsdumont@gmail.com>
2013-05-05 20:28:09 -0300

    Implements filter for getting post summary

--- src/Core/Liquid/LiquidFilters.cs
+++ src/Core/Liquid/LiquidFilters.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RDumont.Frankie.Core.Liquid
 {
@@ -12,6 +13,35 @@ namespace RDumont.Frankie.Core.Liquid
         public static string IsoDate(DateTime date)
         {
             return date.ToString("yyyy-MM-ddTHH:mm:ssZ");
+        }
+
+        public static string Summary(string body, int limit)
+        {
+            var matches = Regex.Matches(body, @"<p.*?>(.+?)</p>",
+                RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+            if (matches.Count == 0) return "";
+
+            // get text and remove line breaks
+            var firstParagraph = matches[0].Groups[1].Value
+                .Trim('\r', '\n', ' ')
+                .Replace("\r", " ")
+                .Replace("\n", " ");
+
+            // remove html
+            firstParagraph = Regex.Replace(firstParagraph, @"<.+?>", "");
+
+            // remove space excess
+            firstParagraph = Regex.Replace(firstParagraph, @"\s+", " ");
+
+            // only break on the next space, so no words get cut
+            var safeLimit = firstParagraph.IndexOfAny(new[] {' ', '\r', '\n'},
+                Math.Min(limit, firstParagraph.Length));
+
+            return (safeLimit < 0
+                ? firstParagraph
+                : firstParagraph.Substring(0, safeLimit))
+                + " (...)";
         }
     }
 }
---
+++ src/Tests/Liquid/FiltersTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using RDumont.Frankie.Core.Liquid;
+
+namespace RDumont.Frankie.Tests.Liquid
+{
+    [TestFixture]
+    public class FiltersTests
+    {
+        public class Summary
+        {
+            [Test]
+            public void One_paragraph()
+            {
+                // Arrange
+                var body = @"bla bla
+<p>
+    Lorem ipsum <strong>dolor sit</strong> amet,
+    consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat.
+</p>
+
+<p>
+    Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+la la la";
+
+                // Act
+                var summary = Filters.Summary(body, 30);
+
+                // Assert
+                Assert.That(summary, Is.EqualTo("Lorem ipsum dolor sit amet, consectetur (...)"));
+            }
+        }
+
+        [Test]
+        public void Shorter_than_the_limit()
+        {
+            // Arrange
+            var body = @"bla bla
+<p>
+    Lorem ipsum <strong>dolor sit</strong> amet,
+    consectetur adipisicing.
+</p>
+
+<p>
+    Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+la la la";
+
+            // Act
+            var summary = Filters.Summary(body, 50);
+
+            // Assert
+            Assert.That(summary,
+                Is.EqualTo("Lorem ipsum dolor sit amet, consectetur adipisicing. (...)"));
+        }
+    }
+}
